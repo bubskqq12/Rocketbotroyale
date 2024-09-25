16f53ef25482a3e5124b639905ef00f973a3f57c
bubskqq12 <ethanjames3713+!@gmail.com>
2024-09-25 17:32:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,31 +2,35 @@ const express = require("express");
 const https = require("https");
 const app = express();
 
-// Proxy request to chess.com but don't show content
+// Proxy request to chess.com but don't show the actual link to the user
 app.get("/chess/*", (req, res, next) => {
   const pathSegments = req.path.split('/');
   const discordPath = pathSegments[2];
-  
-  https.request(new URL("https://chess.com/" + discordPath), (resp) => {
-    // We can pipe the response if needed, but we don't show it to the user
-    // res.contentType(resp.headers["content-type"]);
-    // resp.pipe(res);
-    
-    // Instead, send a blank page
-    res.send("<html><body></body></html>");
-  }).end();
+
+  // Make a request to the external URL
+  https.get("https://chess.com/" + discordPath, (externalRes) => {
+    res.setHeader('Content-Type', externalRes.headers['content-type']);  // Set the content type based on the external response
+
+    // Pipe the external response to the user but keep them on your server's URL
+    externalRes.pipe(res);
+  }).on('error', (err) => {
+    console.error('Error fetching from chess.com:', err);
+    res.status(500).send('Error fetching content');
+  });
 });
 
-// Proxy request to rocketbotroyale2.winterpixel.io but don't show content
+// Proxy request to rocketbotroyale2.winterpixel.io but don't show the actual link to the user
 app.get("/*", (req, res, next) => {
-  https.request(new URL("https://rocketbotroyale2.winterpixel.io/" + req.path), (resp) => {
-    // We can pipe the response if needed, but we don't show it to the user
-    // res.contentType(resp.headers["content-type"]);
-    // resp.pipe(res);
-    
-    // Instead, send a blank page
-    res.send("<html><body></body></html>");
-  }).end();
+  // Make a request to the external URL
+  https.get("https://rocketbotroyale2.winterpixel.io/" + req.path, (externalRes) => {
+    res.setHeader('Content-Type', externalRes.headers['content-type']);  // Set the content type based on the external response
+
+    // Pipe the external response to the user but keep them on your server's URL
+    externalRes.pipe(res);
+  }).on('error', (err) => {
+    console.error('Error fetching from rocketbotroyale2.winterpixel.io:', err);
+    res.status(500).send('Error fetching content');
+  });
 });
 
 app.listen(3000, () => {
